1746014740c64c97765b52daa93c5ad30976a753
Aisawa <sanchezvincent@hotmail.fr>
2026-01-31 15:53:06 +0100

    Init Shared project

---
+++ YgoDeckManager.Shared/DTOs/CardDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YgoDeckManager.Shared.DTOs
+{
+    public class CardDTO
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public List<CardImage> card_images { get; set; }
+    }
+
+    public class CardImage
+    {
+        public string image_url { get; set; }
+        public string image_url_small { get; set; }
+    }
+}
---
+++ YgoDeckManager.Shared/DTOs/CardSetDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YgoDeckManager.Shared.DTOs
+{
+    public class CardSetDTO
+    {
+        public string set_name { get; set; }
+        public string set_code { get; set; }
+        public int num_of_cards { get; set; }
+        public string tcg_date { get; set; }
+    }
+}
